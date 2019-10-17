54e4af2fe302f39b1e068f194d56de62f626b332
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-17 13:28:28 -0700

    COmmit

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,18 +1,20 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score =0;
 		// 2.  Ask the user a question 
-		
+		String answer = JOptionPane.showInputDialog(null, "ok");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (answer.equalsIgnoreCase("ok")) {
 		// 4.  if the user's answer was correct, add one to their score 
 		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		// 5.  Create more questions by repeating steps 2, 3, and 4 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
 		
